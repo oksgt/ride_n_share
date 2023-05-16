4a40286ae52cc8cc060d7e7f9f93315c64140998
Oka Sigit <oka.sigit9@gmail.com>
2023-05-16 17:00:58 +0700

    improvement

---
+++ backend/app/Notifications/LoginNeedsVerification.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+
+class LoginNeedsVerification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    { 
+        return [TwilioChannel::class];
+    }
+
+    public function toTwilio($notifiable)
+    {
+        // generate login code
+        $login_code = rand(111111, 999999);
+
+        // store login code on database
+        $notifiable->update([
+            'login_code' => $login_code
+        ]);
+
+        // send login code to user
+        return (new TwilioSmsMessage())
+            ->content('Your Ride N Share login code is : '.$login_code.' Don\'t share this code with anyone.');
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
